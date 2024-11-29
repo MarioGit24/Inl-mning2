e1f50d801a7b6f097ebce1c1c84d489e43f189ff
mariobugarin <mario00@live.se>
2024-11-29 13:55:18 +0100

    Skapade två tester för BookTest

--- src/test/java/models/BookTest.java
+++ src/test/java/models/BookTest.java
@@ -6,16 +6,27 @@ import org.junit.jupiter.api.Test;
 
 class BookTest {
 
-	//G
-	@Test
-	public void test2EqualBooks() {
+    //G
+    @Test
+    public void test2EqualBooks() {
+        BookCatalogTest bookCatalogTest = new BookCatalogTest();
 
-	}
+        Book book = new Book(123, "Hello", "", "", "", 200);
+        Book book2 = new Book(123, "Hello", "", "", "", 200);
 
-	//G
-	@Test
-	public void test2NonEqualBooks() {
+        assertEquals(book, book2);
 
-	}
+    }
+
+    //G
+    @Test
+    public void test2NonEqualBooks() {
+        BookCatalogTest bookCatalogTest = new BookCatalogTest();
+
+        Book book = new Book(123, "Hello", "", "", "", 200);
+        Book book2 = new Book(321, "hej", "", "", "", 200);
+
+        assertNotEquals(book, book2);
+    }
 
 }
